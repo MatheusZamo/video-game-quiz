b5097610c9d820d603f2295f39dd1b6618b20519
MatheusZamo <19matheushenrique03@gmail.com>
2024-12-30 21:53:04 -0300

    Displaying dynamic messages depending on the hit value

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -3,9 +3,23 @@ const Result = ({ state, maxScore, onClickRestart }) => {
   return (
     <>
       <div className="result">
-        <span>
-          Você fez <b>{state.userScore}</b> pontos de {maxScore} ({percentage}%)
-        </span>
+        {state.userScore === 0 && (
+          <span>
+            😥 Poxa, você fez {state.userScore} pontos de {maxScore} (
+            {percentage}%)
+          </span>
+        )}
+        {state.userScore <= 40 && state.userScore > 0 && (
+          <span>
+            😐 Você fez {state.userScore} pontos de {maxScore} ({percentage}%)
+          </span>
+        )}
+        {state.userScore === maxScore && (
+          <span>
+            🏆 Caramba! Você fez {state.userScore} pontos de {maxScore} (
+            {percentage}%)
+          </span>
+        )}
       </div>
       <button onClick={onClickRestart} className="btn btn-ui">
         Reiniciar quiz
