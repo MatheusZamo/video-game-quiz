a2dec46f78fb92f2877737569c2aeec8cf7ef255
MatheusZamo <19matheushenrique03@gmail.com>
2025-01-01 21:38:48 -0300

    Different result messages depending on the score

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,25 +1,11 @@
+import { getResultMessage } from "@/utils/get-result-message"
+
 const Result = ({ state, maxScore, onClickRestart }) => {
-  const percentage = (state.userScore / maxScore) * 100
+  const resultMessage = getResultMessage({ score: state.userScore, maxScore })
   return (
     <>
       <div className="result">
-        {state.userScore === 0 && (
-          <span>
-            😥 Poxa, você fez {state.userScore} pontos de {maxScore} (
-            {percentage}%)
-          </span>
-        )}
-        {state.userScore <= 40 && state.userScore > 0 && (
-          <span>
-            😐 Você fez {state.userScore} pontos de {maxScore} ({percentage}%)
-          </span>
-        )}
-        {state.userScore === maxScore && (
-          <span>
-            🏆 Caramba! Você fez {state.userScore} pontos de {maxScore} (
-            {percentage}%)
-          </span>
-        )}
+        <span>{resultMessage}</span>
       </div>
       <button onClick={onClickRestart} className="btn btn-ui">
         Reiniciar quiz
