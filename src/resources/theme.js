7bb7396a1e169fc636df2181c38ac8eab607e39d
MatheusZamo <19matheushenrique03@gmail.com>
2025-08-29 14:30:41 -0300

    Refactor theme structure to nest colors within a colors object and fix CSS syntax in Timer component

--- src/resources/theme.js
+++ src/resources/theme.js
@@ -1,9 +1,11 @@
 const theme = {
-  white: "#e8ebee",
-  gray: "#495057",
-  blue: "#29aefd",
-  magenta: "#fd6085",
-  green: "#7befb7",
+  colors: {
+    white: "#e8ebee",
+    gray: "#495057",
+    blue: "#29aefd",
+    magenta: "#fd6085",
+    green: "#7befb7",
+  },
 }
 
 export { theme }
